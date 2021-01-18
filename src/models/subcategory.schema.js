4885d27c4efbc29c3be661ed281890793bc2487a
Sandeep Gupta <sandeepguptabc@gmail.com>
2021-01-18 17:06:56 +0530

    sub cat error handle

--- src/models/subcategory.schema.js
+++ src/models/subcategory.schema.js
@@ -1,29 +1,28 @@
-let ErrorResponse = require("../utils/errorResponse")
-var mongoose = require('mongoose');
+let ErrorResponse = require("../utils/errorResponse");
+var mongoose = require("mongoose");
 var subcategories = new mongoose.Schema({
-    subcategory: {
-        type: String, 
-        required: true,
-        unique: [true, "subcategory already exists"]
-    },
-    category_id: {
-        type: mongoose.Types.ObjectId, 
-        required: true
-    }
-}); 
+	subcategory: {
+		type: String,
+		required: true,
+		unique: [true, "subcategory already exists"],
+	},
+	category_id: {
+		type: mongoose.Types.ObjectId,
+		required: true,
+	},
+});
 
 //before saving check for duplicate
-subcategories.pre("save", async function(next){
-    let allData = await subcats.find({})
-    allData.forEach(data => {
-        if (data.subcategory.toLowerCase() === this.subcategory.toLowerCase()) {
-            throw new ErrorResponse("Resourse Already Exist.", 400)   
-        }
-    })
-    next()
-})
+subcategories.pre("save", async function (next) {
+	let allData = await subcats.find({});
+	allData.forEach((data) => {
+		if (data.subcategory.toLowerCase() === this.subcategory.toLowerCase()) {
+			next(new ErrorResponse("Resourse Already Exist.", 400));
+		}
+	});
+	next();
+});
 
-let subcats = mongoose.model("subcategories", subcategories)
+let subcats = mongoose.model("subcategories", subcategories);
 
-
-module.exports = subcats
+module.exports = subcats;
