106e600c1dd75f86e4422d4a3041302f2b5dc175
Sandeep Gupta <sandeepguptabc@gmail.com>
2021-01-28 17:52:03 +0530

    pagination

--- src/utils/paginator.js
+++ src/utils/paginator.js
@@ -1,69 +1,79 @@
 const { getModel } = require("../models");
 const ErrorResponse = require("./errorResponse");
 
-module.exports = async (Model, reqQuery, path=null, select="-__v -_id") => {
+module.exports = async (Model, reqQuery, path = null, select = "-__v -_id") => {
+	Model = getModel(Model);
 
-    Model = getModel(Model)
+	let filters = { ...reqQuery };
+	let query;
 
-    let filters = { ...reqQuery }
-    let query;
+	//fiels we dont need in filtering
+	let removeFields = ["select", "sort", "page", "limit", "keyword"];
+	removeFields.forEach((param) => delete filters[param]);
 
-    //fiels we dont need in filtering
-    let removeFields = ["select", "sort", "page", "limit"]
-    removeFields.forEach( (param) => delete filters[param] )
+	let queryStr = JSON.stringify(filters);
+	queryStr = queryStr.replace(
+		/\b(gt|gte|lt|lte|in)\b/g,
+		(match) => `$${match}`
+	);
 
-    let queryStr = JSON.stringify(filters)
-    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
+	query = Model.find({
+		...JSON.parse(queryStr),
+		title: { $regex: reqQuery.keyword ? reqQuery.keyword : "", $options: "i" },
+	});
+	if (path) {
+		query.populate({ path: path, select: select });
+	}
 
-    // console.log()
+	let pagination = {};
+	let data;
 
-    query = Model.find(JSON.parse(queryStr))
-    if (path) {
-        query.populate({path: path, select:select})
-    }
+	//pagination
+	let page = parseInt(reqQuery.page, 10) || 1;
+	let limit = parseInt(reqQuery.limit, 10) || 6;
+	let startIndex = (page - 1) * limit;
+	let endIndex = page * limit;
+	let total = await Model.countDocuments();
 
-    let pagination = {}
-    let data;
+	if (endIndex < total) {
+		pagination.next = {
+			page: page + 1,
+			limit,
+		};
+	}
 
-    //pagination
-    let page = parseInt(reqQuery.page, 10) || 1
-    let limit = parseInt(reqQuery.limit, 10) || 3
-    let startIndex = (page - 1) * limit
-    let endIndex = page * limit
-    let total = await Model.countDocuments() 
+	if (startIndex > 0) {
+		pagination.prev = {
+			page: page - 1,
+			limit,
+		};
+	}
 
-    if ( endIndex < total ) {
-        
-        pagination.next = {
-            page: page+1,
-            limit
-        }
-    }
+	query = query.skip(startIndex).limit(limit);
 
-    if (startIndex > 0) {
-        pagination.prev = {
-            page: page-1,
-            limit
-        }
-    }
+	//if any select opertaions
+	if (reqQuery.select) {
+		let fields = reqQuery.select.split(",").join(" ");
+		query.select(fields);
+	}
 
-    query = query.skip(startIndex).limit(limit)
+	//if any sort operation
+	if (reqQuery.sort) {
+		let sortBy = reqQuery.sort.split(",").join(" ");
+		query = query.sort(sortBy);
+	}
 
-    //if any select opertaions
-    if (reqQuery.select) {
-        let fields = reqQuery.select.split(",").join(" ")
-        query.select(fields)
-    }
-
-    //if any sort operation
-    if (reqQuery.sort) {
-        let sortBy = reqQuery.sort.split(",").join(" ")
-        query = query.sort(sortBy)
-    }
-
-    data = await query
-    if(!data || !data.length){
-        throw new ErrorResponse("No Data Found!!", 404)
-    }
-    return { pagination, data }
-}
+	data = await query;
+	let count = await Model.countDocuments({
+		...JSON.parse(queryStr),
+		title: {
+			$regex: reqQuery.keyword ? reqQuery.keyword : "",
+			$options: "i",
+		},
+	});
+	pagination.pages = Math.ceil(count / limit);
+	if (!data || !data.length) {
+		throw new ErrorResponse("No Data Found!!", 404);
+	}
+	return { pagination, data };
+};
