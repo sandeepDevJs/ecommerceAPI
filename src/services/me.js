bf143cb0d0a1ff1fd9ba5ceede439b07ed56cf8b
Sandeep Gupta <sandeepguptabc@gmail.com>
2021-01-28 21:54:24 +0530

    Me Route detailed update

--- src/services/me.js
+++ src/services/me.js
@@ -1,15 +1,22 @@
 const asyncHandler = require("../api/middlewares/asyncHandler");
 const crudOPs = require("../models");
+const userModel = require("../models/users.schema");
+const { generateToken } = require("../utils/jwt");
 
 module.exports.getMe = asyncHandler(async (req, res) => {
 	let id = req.userData.id;
 	let data = await crudOPs.getData("users", id);
-	return res.send({ success: true, data });
+	let token = generateToken(data._doc);
+	return res.send({ success: true, data: { ...data._doc, token } });
 });
 
 module.exports.updateMe = asyncHandler(async (req, res) => {
 	let id = req.userData.id;
 	let reqData = { ...req.body };
-	let data = await crudOPs.updateData("users", id, reqData);
-	return res.send({ success: true, data });
+	let data = await userModel.findByIdAndUpdate(id, reqData, {
+		new: true,
+		runValidators: true,
+	});
+	let token = generateToken(data._doc);
+	return res.send({ success: true, data: { ...data._doc, token } });
 });
