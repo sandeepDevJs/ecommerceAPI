9daef228172279e1439a77c649fc994a33ab646a
Sandeep Gupta <sandeepguptabc@gmail.com>
2021-02-09 13:47:19 +0530

    noSql injectection protection

--- src/loaders/middlewares/index.js
+++ src/loaders/middlewares/index.js
@@ -8,11 +8,13 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
 const path = require("path");
+const mongoSanitize = require("express-mongo-sanitize");
 
 module.exports = (express, app) => {
 	app.use(express.json());
 	app.use(morgan("dev"));
 	app.use(helmet());
 	app.use(cors());
+	app.use(mongoSanitize());
 	app.use("/api/images", express.static("src/uploads/"));
 };
