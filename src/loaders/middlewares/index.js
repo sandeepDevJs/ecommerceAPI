a14d8fec9e2e068fe242c0ed1e9204f48fb1223a
Sandeep Gupta <sandeepguptabc@gmail.com>
2021-02-09 14:10:28 +0530

    Rate limiting, hpp

--- src/loaders/middlewares/index.js
+++ src/loaders/middlewares/index.js
@@ -7,14 +7,33 @@
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
-const path = require("path");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
 const mongoSanitize = require("express-mongo-sanitize");
 
 module.exports = (express, app) => {
 	app.use(express.json());
 	app.use(morgan("dev"));
+
+	//header protection
 	app.use(helmet());
+
+	//CORS
 	app.use(cors());
+
+	//sanitization
 	app.use(mongoSanitize());
+
+	//xss protection
+	app.use(xss());
+
+	//Rate Limiting
+	let limiter = rateLimit({ windowMs: 10 * 60 * 1000, max: 100 });
+	app.use(limiter);
+
+	//prevent http param pollution
+	app.use(hpp());
+
 	app.use("/api/images", express.static("src/uploads/"));
 };
